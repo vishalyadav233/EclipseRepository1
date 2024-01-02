2fbbdcb8fda0236a22f1d331ae76bea409f660c7
Vishal <vishalimp2002@gmail.com>
2024-01-02 08:15:55 +0530

    Lambda Expression

---
+++ src/p1/A.java
@@ -0,0 +1,4 @@
+package p1;
+public interface  A {
+	public void add(int m , int n);
+}
---
+++ src/p1/B.java
@@ -0,0 +1,28 @@
+package p1;
+public class B {
+	public static void main(String[] args) {
+		A a1 = (int x , int y)->{
+			int z = x+y;
+			System.out.println(z);
+			
+		};
+		a1.add(10,20);
+	}
+	C c1 = ( int x , int y)->{
+		int z = x*y;
+		System.out.println(z);
+		
+	};
+
+
+	
+	
+}
+
+	
+	
+
+
+
+
+
